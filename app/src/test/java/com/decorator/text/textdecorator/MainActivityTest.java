831866d198df0bf7dbfe5109fcc5df6122c81362
mihail <urmanschi.mihail@gmail.com>
2016-11-07 20:22:04 +0200

    added test test

---
+++ app/src/test/java/com/decorator/text/textdecorator/MainActivityTest.java
@@ -0,0 +1,17 @@
+package com.decorator.text.textdecorator;
+
+import android.support.v4.media.MediaMetadataCompat;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MainActivityTest {
+
+
+    @Test
+    public void testTest(){
+        assertEquals(1,1);
+    }
+
+}
